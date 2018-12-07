f4603fc3684f5d23d8c29d7403685b08c564df41
MikeO <ozman4@gmail.com>
2018-12-07 02:03:37 -0600

    Not all the way there just need to calculate returns

--- generateData.py
+++ generateData.py
@@ -15,22 +15,39 @@ def populate_factors(n):
 def get_std_devs(factors):
 	std_devs = []
 	for i in factors:
+		# Sampling from adding a normal and a uniform distribution to get the standard deviations for each factor
 		v = np.random.normal(0,.05,1) + np.random.uniform(0.1,0.25,1)
 		std_devs.append(v)
-		# v = random()
-
-		# scaled_value = 0.1 + (v * (0.3 - 0.1))
-		# std_devs.append(scaled_value)
 	return std_devs
 
 
 # Samples from a normal distribution n-times to get the pct change for n-number of data points using the sigma values generated
 def get_pct_changes(n,std_devs):
 	p = []
-	for i in std_devs:
-		s = np.random.normal(0,i,n)
+	for dev in std_devs:
+		# s is a list, so pct changes is a list of lists
+		s = np.random.normal(0,dev,n)
 		p.append(s)
 	return p
+
+
+# Calculates the returns on index based on time-units
+def get_returns(pct_changes,numDataPoints):
+	rets = []
+	
+	num_returns = numDataPoints
+	i = 0
+	while(i is not num_returns):
+		summing = 0
+		for factor_change in pct_changes:
+			w = np.random.uniform(0.2,1)
+			summing+=w*factor_change[i]
+			# print(factor_change[i])
+		rets.append(summing)
+		i+=1
+	return rets
+
+
 
 def write_csv():
 	myData = [factors,pct_changes]
@@ -40,22 +57,26 @@ def write_csv():
 		writer.writerows(myData)
 
 
-
-
-
 numFactors=100
-numDataPoints = 100
+numDataPoints = 1000
 
 
 factors = populate_factors(numFactors)
 std_devs = get_std_devs(factors)
 pct_changes = get_pct_changes(numDataPoints,std_devs)
-write_csv()
-# weights = []
-# returns = []
+# for row in pct_changes:
+# 	print(row[0])
 
-# write
+# print()
+returns = get_returns(pct_changes,numDataPoints)
+print(max(returns))
+print(min(returns))
+print(sum(returns)/len(returns))
+# for l in pct_changes:
+# 	print(l)
 
+# returns = get_returns(pct_changes,numDataPoints)
+# write_csv()
 
 
 
